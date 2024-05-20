189f3d6225bb2c41ca91b852d953ae0c3837d65c
Rogov Dmitriy <RogovDR@mpei.ru>
2024-05-20 12:19:22 +0300

    Промежуточный коммит

--- app.js
+++ app.js
@@ -233,13 +233,28 @@ const abi = [
 ];
 
 const contract = new web3.eth.Contract(abi, contractAddress);
-let accounts = [];
+let isConnectedMetamask = false;
+let candidates = [];
+let candidate;
+let currentAccount;
+let candidatesBlock = document.querySelector("#candidatesBlock");
 let metamaskBtn = document.querySelector("#metamaskBtn");
 let voteBtn = document.querySelector("#voteBtn");
 console.log(contract);
 
+function showLoader() {
+    document.querySelector("body > div > div").style.opacity='0.6';
+    document.querySelector(".loader").style.display='block';
+}
+
+function disableLoader() {
+    document.querySelector("body > div > div").style.opacity='1';
+    document.querySelector(".loader").style.display='none';
+}
+
 
 window.onload = async function() {
+    
     //await connectMetamask();
     metamaskBtn.addEventListener("click",async function(){
         //check metamask is installed
@@ -248,71 +263,139 @@ window.onload = async function() {
             const web3 = new Web3(window.ethereum);
 
             //request user to connect accounts (Metamask will prompt)
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            try {
+                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                currentAccount = accounts[0];
+                isConnectedMetamask = true;
+            } catch(err) {
+                console.log(err);
+                if (err.code === 4001) return
+            }
 
-            //get the connected accounts
-            accounts = await web3.eth.getAccounts();
-            console.log(accounts.length);
+            if (isConnectedMetamask) {
+                metamaskBtn.disabled=true;
+                const owner = await contract.methods.getOwner().call();
+
+                if (currentAccount.split('0x')[1].toUpperCase() == owner.split('0x')[1]) {
+                    document.querySelector("div.adminBlock").classList.remove('adminBlock')
+                    return;
+                }
+
+                //Добавить проверку, что уже голосовал
+
+                const candidatesCount = await contract.methods.getCandidatesCount().call();
+                for (let i=1; i<=candidatesCount; i++) {
+
+                    candidate = await contract.methods.getCandidate(i).call();
+                    candidates.push({
+                        name: candidate,
+                        id: i
+                    })
+                    candidatesBlock.innerHTML+=
+                    `<div class='d-flex justify-content-between'>
+                        <p class='text-default fs-5 mb-0'>`+candidate+`</p><input type='checkbox' id="`+i+`">
+                    </div>`;
+                    document.querySelector("body > div > div > div > h2:nth-child(3)").style.display='block';
+                    voteBtn.style.display='block';
+
+                    document.querySelector("#candidatesBlock").querySelectorAll('input').forEach(function(el){
+                        el.addEventListener("click",function(){
+                            if (el.checked) {
+                                document.querySelector("#candidatesBlock").querySelectorAll('input').forEach(function(el){
+                                    el.disabled=true;
+                                })
+                            }
+                            
+                        })
+                    })
+
+                }
+            }
+            
         } else {
             alert('Please download metamask');
         }
     });
 
     voteBtn.addEventListener("click", function() {
-        //обращение к контракту если все норм проголосовалось то:
+        
+        if (isConnectedMetamask) {
+
+            document.querySelector("#candidatesBlock").querySelectorAll('input').forEach(function(el){
+                if (el.checked) {
+                    showLoader();
+                    contract.methods.vote(el.id).send({from: currentAccount, gas: 200000})
+                        .on('transactionHash', function(hash){
+                            // Когда транзакция отправлена, вы можете выполнить дополнительные действия
+                            console.log('Transaction hash:', hash);
+                        })
+                    .on('confirmation', function(confirmationNumber, receipt){
+                            // Когда транзакция подтверждена, вы можете выполнить дополнительные действия
+                            disableLoader();
+                            document.querySelector("body > div > div > div").querySelectorAll("h2,div,button").forEach(el=>el.style.display='none')
+                            document.querySelector("div.alert.alert-success").style.display='block';
+                            console.log('Confirmation number:', confirmationNumber);
+                        })
+                    .on('error', function(err){
+                            disableLoader();
+                            console.log(err);
+                    })
+                }
+            })
+
+        }
 
     })
 
-    const owner = await contract.methods.getOwner().call().then(console.log);
-    const candidatesCount = await contract.methods.getCandidatesCount().call();
+    
     const votesCount = await contract.methods.getVotesCount().call();
     const electionsEnd = await contract.methods.getElectionsEnd().call();
 
-    document.getElementById('owner').innerText = `Owner: ${owner}`;
-    document.getElementById('candidatesCount').innerText = `Candidates Count: ${candidatesCount}`;
-    document.getElementById('votesCount').innerText = `Votes Count: ${votesCount}`;
-    document.getElementById('electionsEnd').innerText = `Elections End: ${electionsEnd}`;
+    // document.getElementById('owner').innerText = `Owner: ${owner}`;
+    // document.getElementById('candidatesCount').innerText = `Candidates Count: ${candidatesCount}`;
+    // document.getElementById('votesCount').innerText = `Votes Count: ${votesCount}`;
+    // document.getElementById('electionsEnd').innerText = `Elections End: ${electionsEnd}`;
 
-    const getCandidates = async () => {
-        const candidatesDiv = document.getElementById('candidates');
-        candidatesDiv.innerHTML = ''; // Clear previous candidates
+    // const getCandidates = async () => {
+    //     const candidatesDiv = document.getElementById('candidates');
+    //     candidatesDiv.innerHTML = ''; // Clear previous candidates
 
-        for (let i = 1; i <= candidatesCount; i++) {
-            const candidate = await contract.methods.candidates(i).call();
-            candidatesDiv.innerHTML += `<div>Candidate ${i}: ${candidate.name} - Votes: ${candidate.voteCount}</div>`;
-        }
-    };
+    //     for (let i = 1; i <= candidatesCount; i++) {
+    //         const candidate = await contract.methods.candidates(i).call();
+    //         candidatesDiv.innerHTML += `<div>Candidate ${i}: ${candidate.name} - Votes: ${candidate.voteCount}</div>`;
+    //     }
+    // };
 
-    window.getCandidates = getCandidates;
+    // window.getCandidates = getCandidates;
 
-    const addCandidate = async () => {
-        const candidateName = document.getElementById('candidateName').value;
-        await contract.methods.addCandidate(candidateName).send({from: web3.eth.defaultAccount});
-        alert('Candidate added!');
-    };
+    // const addCandidate = async () => {
+    //     const candidateName = document.getElementById('candidateName').value;
+    //     await contract.methods.addCandidate(candidateName).send({from: web3.eth.defaultAccount});
+    //     alert('Candidate added!');
+    // };
 
-    window.addCandidate = addCandidate;
+    // window.addCandidate = addCandidate;
 
-    const vote = async () => {
-        const candidateId = document.getElementById('candidateId').value;
-        await contract.methods.vote(candidateId).send({from: web3.eth.defaultAccount});
-        alert('Voted!');
-    }
+    // const vote = async () => {
+    //     const candidateId = document.getElementById('candidateId').value;
+    //     await contract.methods.vote(candidateId).send({from: web3.eth.defaultAccount});
+    //     alert('Voted!');
+    // }
 
-    window.vote = vote;
+    // window.vote = vote;
 
-    const endElections = async () => {
-        await contract.methods.endElections().send({from: web3.eth.defaultAccount});
-        document.getElementById('electionsEnd').innerText = `Elections End: ${true}`;
-        alert('Elections ended!');
-    };
+    // const endElections = async () => {
+    //     await contract.methods.endElections().send({from: web3.eth.defaultAccount});
+    //     document.getElementById('electionsEnd').innerText = `Elections End: ${true}`;
+    //     alert('Elections ended!');
+    // };
 
-    window.endElections = endElections;
+    // window.endElections = endElections;
 
-    const getWinner = async () => {
-        const winner = await contract.methods.getWinner().call();
-        document.getElementById('winner').innerText = `Winner: ${winner}`;
-    };
+    // const getWinner = async () => {
+    //     const winner = await contract.methods.getWinner().call();
+    //     document.getElementById('winner').innerText = `Winner: ${winner}`;
+    // };
 
-    window.getWinner = getWinner;
+    // window.getWinner = getWinner;
 };
